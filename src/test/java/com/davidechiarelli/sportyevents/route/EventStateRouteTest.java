1dfd51d9f54c6eb128786a9fcc43535f9a13b47b
Davide Chiarelli <davide.chiarelli@vodafone.com>
2025-10-31 20:08:37 +0100

    refactor: used testcontainer for camel integration tests

--- src/test/java/com/davidechiarelli/sportyevents/route/EventStateRouteTest.java
+++ src/test/java/com/davidechiarelli/sportyevents/route/EventStateRouteTest.java
@@ -1,26 +1,36 @@
 package com.davidechiarelli.sportyevents.route;
 
+import com.davidechiarelli.sportyevents.dto.EventError;
+import com.davidechiarelli.sportyevents.dto.EventScore;
 import com.davidechiarelli.sportyevents.entity.EventStatusEntity;
 import com.davidechiarelli.sportyevents.repository.EventStatusRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.camel.CamelContext;
-import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.impl.engine.DefaultShutdownStrategy;
-import org.apache.camel.spi.ShutdownStrategy;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
-import org.junit.jupiter.api.AfterEach;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.Map;
 
 import static org.apache.camel.builder.AdviceWith.adviceWith;
-import static org.apache.camel.test.junit5.TestSupport.getMockEndpoint;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_OFFSET_RESET_CONFIG;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
 
@@ -29,69 +39,73 @@ import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER
 @AutoConfigureWireMock(port = 0, stubs = {"classpath:/mappings"})
 @ActiveProfiles("test")
 @DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 class EventStateRouteTest {
 
-    @Autowired
-    protected CamelContext camelContext;
 
     @MockitoBean
     private EventStatusRepository eventStatusRepository;
 
-    private MockEndpoint mockedKafka;
-    private MockEndpoint mockedDlqKafka;
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafka;
+
+    @Autowired
+    ProducerTemplate producerTemplate;
+
+    @Autowired
+    CamelContext camelContext;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Consumer<String, String> consumer;
 
     @BeforeEach
     void setUp() throws Exception {
 
-        ShutdownStrategy shutdownStrategy = new DefaultShutdownStrategy();
-        shutdownStrategy.setTimeout(10);
-        shutdownStrategy.setShutdownNowOnTimeout(true);
-        shutdownStrategy.setLogInflightExchangesOnTimeout(true);
-        shutdownStrategy.setSuppressLoggingOnTimeout(true);
-        camelContext.setShutdownStrategy(shutdownStrategy);
+        adviceWith(camelContext, "route-scheduler", routeBuilder ->
+                routeBuilder.replaceFromWith("direct:triggerScheduler"));
 
-        adviceWith(camelContext, "route-send-event-score", routeBuilder ->
-                routeBuilder
-                        .weaveByToUri("kafka:*")
-                        .replace()
-                        .to("mock:kafka")
-        );
+        camelContext.start();
 
-        adviceWith(camelContext, "route-send-event-score-to-dlq", routeBuilder ->
-                routeBuilder
-                        .weaveByToUri("kafka:*")
-                        .replace()
-                        .to("mock:kafka-dlq")
-        );
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("testGroup", "true", embeddedKafka);
+        consumerProps.put(AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        mockedKafka = getMockEndpoint(camelContext, "mock:kafka", true);
-        mockedDlqKafka = getMockEndpoint(camelContext, "mock:kafka-dlq", true);
+        consumer = new KafkaConsumer<>(consumerProps, new StringDeserializer(), new StringDeserializer());
+
+        consumer.subscribe(List.of("eventScores", "eventScores_dlq"));
     }
 
-    @AfterEach
-    void cleanUp() {
-
-        camelContext.stop();
-    }
 
     @Test
-    void shouldIngestEventStatus() throws InterruptedException {
+    void shouldIngestEventStatus() throws Exception {
 
         when(eventStatusRepository.findAllByStatusIsTrue()).thenReturn(List.of(new EventStatusEntity("1", true)));
 
-        mockedDlqKafka.expectedMinimumMessageCount(1);
-        mockedKafka.expectedBodiesReceived("{\"eventId\":\"1\",\"currentScore\":\"10:1\"}");
-        mockedKafka.assertIsSatisfied();
+        producerTemplate.sendBody("direct:triggerScheduler", null);
+
+        var kafkaRecord = KafkaTestUtils.getSingleRecord(consumer, "eventScores", Duration.ofSeconds(5));
+        assertThat(kafkaRecord).isNotNull();
+
+        var eventScore = objectMapper.readValue(kafkaRecord.value(), EventScore.class);
+        assertThat(eventScore.eventId()).isEqualTo("1");
+        assertThat(eventScore.currentScore()).isEqualTo("10:1");
     }
 
     @Test
-    void shouldNotIngestEventStatusForException() throws InterruptedException {
+    void shouldNotIngestEventStatusForException() throws Exception {
 
         when(eventStatusRepository.findAllByStatusIsTrue()).thenReturn(List.of(new EventStatusEntity("2", true)));
 
-        mockedDlqKafka.expectedMinimumMessageCount(1);
-        mockedDlqKafka.message(0).body().contains("{\"eventId\":\"2\",\"errorMessage\":\"HTTP operation failed invoking");
-        mockedDlqKafka.assertIsSatisfied();
-    }
+        producerTemplate.sendBody("direct:triggerScheduler", null);
 
+        assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> KafkaTestUtils.getSingleRecord(consumer, "eventScores", Duration.ofSeconds(5)));
+
+        var kafkaRecord = KafkaTestUtils.getSingleRecord(consumer, "eventScores_dlq", Duration.ofSeconds(5));
+        assertThat(kafkaRecord).isNotNull();
+
+        var eventError = objectMapper.readValue(kafkaRecord.value(), EventError.class);
+        assertThat(eventError.eventId()).isEqualTo("2");
+        assertThat(eventError.errorMessage()).contains("HTTP operation failed invoking");
+    }
 }
